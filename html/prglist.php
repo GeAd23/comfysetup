7c98fa5ca84eb655a0dde5a7cc072a240f39f0c2
Christopher Lemke <ludwiglemke@bitbucket.org>
2021-04-28 14:36:13 +0200

    ICH BIN DER CUMMIE MANN

--- html/prglist.php
+++ html/prglist.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', true));
+ini_set('display_errors', true);
 session_start();
 if(!(isset($_SESSION["timer"])))
 {
@@ -90,8 +90,72 @@ else
     
     <div id="info">
         <div id="usrprglist">
+        <?php
+        $proarray = shell_exec((escapeshellcmd('/var/www/scripts/getprglist.py')));
+        $i = 1;
+        echo '<script language="javascript" type="text/javascript">';
+        echo 'var deactivate = false;';
+        echo '</script>';
+        foreach($proarray as $item){
+            echo '<script language="javascript" type="text/javascript">';
+            echo 'function programm_bearbeitenD'.$i.'(){
+                if(deactivate == false)
+                {
+                        var name = '.$item["name"].';
+                
+                }
+                else
+                {
+                    deactivate = false;
+                }
+            }
             
-        </div>
+            function programm_aktuell'.$i.'(){
+                deactivate = true;
+                var name = '.$item["name"].';
+            
+            }
+            
+            function programm_download'.$i.'(){
+                deactivate = true;
+                var name = '.$item["name"].';
+            
+            }
+            
+            function programm_bearbeiten'.$i.'(){
+                deactivate = true;
+                var name = '.$item["name"].';
+            
+            }
+            
+            function programm_loeschen'.$i.'(){
+                deactivate = true;
+                var name = '.$item["name"].';
+            
+            }';
+            echo '</script>';
+            echo '<div onclick="programm_bearbeitenD'.$i.'()" id="'.$item["name"].'" class="programm">
+            <p id="inhalt">
+            <b>'.$item["name"].'</b>&nbsp;&nbsp;&nbsp;Datum+Zeit&nbsp;&nbsp;&nbsp;
+            <button onclick="programm_aktuell'.$i.'()" id="'.$item["name"].'">
+            Aktuell
+            </button>
+            <button onclick="programm_download'.$i.'()" id="'.$item["name"].'">
+            Herunterladen
+            </button>
+            <button onclick="programm_bearbeiten'.$i.'()" id="'.$item["name"].'">
+            Bearbeiten
+            </button>
+            <button onclick="programm_loeschen'.$i.'()" id="'.$item["name"].'">
+            Löschen
+            </button>
+            </p>
+            </div>';
+            $i = $i+1;
+        }
+        unset($item);
+        ?>
+         </div>
     </div>
 
 
