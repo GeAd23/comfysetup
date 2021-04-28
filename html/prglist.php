b19aa51d13f6c6786bfa2ce8f9df292dae5febe6
Christopher Lemke <ludwiglemke@bitbucket.org>
2021-04-28 14:28:10 +0200

    ICH BIN DER CUMMIE MANN

--- html/prglist.php
+++ html/prglist.php
@@ -1,12 +1,43 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', true));
 session_start();
 if(!(isset($_SESSION["timer"])))
 {
     header("location: login1.php");
     exit();
 }
+if(isset($_SESSION["timer"])){
+if($_SESSION["timer"]+1800 <= time())
+{
+	if($_SESSION["username"] != "")
+	{	
+		if($_SESSION["admin"] == true)
+		{
+			$_SESSION["timer"] = time();
+			
+		}
+		else
+		{
+			$_SESSION["timer"] = time();
+			
+		}
+	}
+	else
+	{
+		include logout.php;
+	}
+}
+else
+{
+	include logout.php;
+}
+}
+else
+{
+    include logout.php;
+}
 ?>
-
 
 <!DOCTYPE html>
 <html de>
@@ -15,10 +46,11 @@ if(!(isset($_SESSION["timer"])))
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--Own stylesheet-->
 <link rel="stylesheet" href="./stylesheets/light.css">
-<link rel="stylesheet" href="./stylesheets/div_Liste.css">
 <!--Fonts-->
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho+B1:wght@400;700&display=swap" rel="stylesheet">
+<!--Icons-->
+<link href='https://css.gg/css' rel='stylesheet'>
 <!--Navbar js-->
 <script src="./js/nav.js"></script>
 </head>
@@ -28,7 +60,7 @@ if(!(isset($_SESSION["timer"])))
         <!--Inhalt der Navigationsleiste-->
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <a href="./index.php">Home</a><br>
-        <a>Konto</a><br>
+        <a href="./account.php">Konto</a><br>
             <a class="haltdeinefressejulien" href="">Programm Liste</a><br>
             <a class="haltdeinefressejulien" href="">Profil Liste</a><br>
             <a class="haltdeinefressejulien" href="">Profil erstellen</a><br>
@@ -37,11 +69,15 @@ if(!(isset($_SESSION["timer"])))
         <a href="./about.php">About</a><br>
         <a href="./help.php">Help</a>
         <?php
-            if (isset($_SESSION["timer"]))
+        if(isset($_SESSION["timer"]))
             {
                 echo '<a class="logout" href="logout.php">Logout</a>';
             }
-        ?>
+            else
+            {
+                echo '<a class="log_in" href="login1.php">Login</a>';
+            }
+        ?> 
     </div>
 
     <div id="side_bar" class="sidebar">
@@ -53,7 +89,9 @@ if(!(isset($_SESSION["timer"])))
     </div>
     
     <div id="info">
-
+        <div id="usrprglist">
+            
+        </div>
     </div>
 
 
---
+++ scripts/getprglist.py
@@ -0,0 +1,13 @@
+import sqlite3 as sql
+import sys
+
+db_conn = sql.connect('../data/MS1.db')
+
+def getprglist():
+    conn=db_conn
+    c=conn.cursor()
+    c.execute(f"SELECT * FROM programm")
+    prg_array = c.fetchall()
+    return prg_array
+
+getprglist()
