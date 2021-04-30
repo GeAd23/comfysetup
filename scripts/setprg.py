faf4fdac52b256d82030dafef29fe5deac5690e7
Christopher Lemke <ludwiglemke@bitbucket.org>
2021-04-30 13:41:38 +0200

    pepega

--- scripts/setprg.py
+++ scripts/setprg.py
@@ -3,14 +3,14 @@ import sys
 import json
 
 db_conn=sql.connect('/var/www/data/MS1.db', timeout=20)
-arg_list=sys.argv[1]
+arg_list=sys.argv
 prg_array = json.load(arg_list)
 
 def setprg(prg_array):
     conn = db_conn
     c = conn.cursor()
     c.execute(f"""INSERT INTO programm(name, url, localurl, program_bild, standard_P, os, last_update, savestate, automatic) 
-                    # VALUES '{prg_array[0]}', '{prg_array[1]}', '{prg_array[2]}', '{prg_array[3]}', '{prg_array[4]}', '{prg_array[5]}', '{prg_array[6]}', '{prg_array[7]}', '{prg_array[8]}', '{prg_array[9]}', '{prg_array[10]}'""")
+                    # VALUES '{prg_array[1]}', '{prg_array[2]}', '{prg_array[3]}', '{prg_array[4]}', '{prg_array[5]}', '{prg_array[6]}', '{prg_array[7]}', '{prg_array[8]}', '{prg_array[9]}', '{prg_array[10]}', '{prg_array[11]}'""")
     conn.commit()
     conn.close()
 
