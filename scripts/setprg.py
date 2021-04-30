79eb8ae2ba74a3f89a044c57cba7bed48872d01a
Christopher Lemke <ludwiglemke@bitbucket.org>
2021-04-30 13:44:42 +0200

    pepega

--- scripts/setprg.py
+++ scripts/setprg.py
@@ -10,7 +10,7 @@ def setprg(prg_array):
     conn = db_conn
     c = conn.cursor()
     c.execute(f"""INSERT INTO programm(name, url, localurl, program_bild, standard_P, os, last_update, savestate, automatic) 
-                    # VALUES '{prg_array[1]}', '{prg_array[2]}', '{prg_array[3]}', '{prg_array[4]}', '{prg_array[5]}', '{prg_array[6]}', '{prg_array[7]}', '{prg_array[8]}', '{prg_array[9]}', '{prg_array[10]}', '{prg_array[11]}'""")
+                     VALUES '{prg_array[1]}', '{prg_array[2]}', '{prg_array[3]}', '{prg_array[4]}', '{prg_array[5]}', '{prg_array[6]}', '{prg_array[7]}', '{prg_array[8]}', '{prg_array[9]}', '{prg_array[10]}', '{prg_array[11]}'""")
     conn.commit()
     conn.close()
 
