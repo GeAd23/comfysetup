19c3706823706ef65891caafdd74f7e6333a7d99
Christopher Lemke <ludwiglemke@bitbucket.org>
2021-04-30 13:37:31 +0200

    pepega

--- scripts/setprg.py
+++ scripts/setprg.py
@@ -1,16 +1,16 @@
 import sqlite3 as sql
 import sys
+import json
 
 db_conn=sql.connect('/var/www/data/MS1.db', timeout=20)
 arg_list=sys.argv[1]
-arg_list=arg_list.replace("(", "")
-arg_list=arg_list.replace(")", "")
+prg_array = json.load(arg_list)
 
-def setprg(arg_list):
+def setprg(prg_array):
     conn = db_conn
     c = conn.cursor()
     c.execute(f"""INSERT INTO programm(name, url, localurl, program_bild, standard_P, os, last_update, savestate, automatic) 
-                    VALUES {arg_list}""")
+                    # VALUES '{prg_array[0]}', '{prg_array[1]}', '{prg_array[2]}', '{prg_array[3]}', '{prg_array[4]}', '{prg_array[5]}', '{prg_array[6]}', '{prg_array[7]}', '{prg_array[8]}', '{prg_array[9]}', '{prg_array[10]}'""")
     conn.commit()
     conn.close()
 
