dd1eb90f2787326c54a1999b4baafd1479bf9be2
ludwiglemke <christopher.lemke@outlook.com>
2021-05-05 17:59:15 +0200

    aaah

---
+++ scripts/chkonto.py
@@ -0,0 +1,16 @@
+import sqlite3 as sql
+import sys
+import json
+
+db_conn=sql.connect('/var/www/data/MS1.db', timeout=20)
+arg_list=sys.argv[1:]
+user = json.dump(arg_list)
+
+def update_profile(name, username, password):
+    conn=db_conn
+    c = conn.cursor()
+    c.exceute(f"UPDATE users SET name='{name}', password='{password}' WHERE username='{username}'")
+    conn.commit()
+    conn.close()
+
+update_profile(user[0], user[1], user[2])
