02cd85ccafa65a67cb71d44f91fc76c53e2e3fca
Timo Hollwedel <hollwedel@univention.de>
2021-05-06 10:41:56 +0200

    Hinzufügen Autogui Script Demo

---
+++ scripts/autogui/autogui_irfanview_demo.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+#Zum Ausführen sind aktuell Adminrechte für das Script auf dem Zielrechner nötig
+from pywinauto import Application
+import ctypes, sys
+import sqlite3 as sql
+
+db_conn = sql.connect('/home/thollwed/Dokumente/Berufsschule/Mittelstufenprojekt/NSDD/data/MS1.db')
+
+def get_db_values():
+    conn = db_conn
+    c = conn.cursor()
+    c.execute(f'SELECT id, path FROM autogui')
+    path_values = c.fetchall()
+    c.execute(f'SELECT id, name FROM autogui')
+    name_values = c.fetchall()
+    c.execute(f'SELECT id, buttons FROM autogui')
+    button_values = c.fetchall()
+    return path_values, name_values, button_values
+
+
+""" Check für Adminrechte
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False """
+
+
+def set_path(path_values):
+    path = path_values[0][1]
+    #path = r"C:\test\iview456g_x64_setup.exe"
+    return path
+
+
+def set_prog_dialog(name_values):
+    prog_dialog = name_values[0][1]
+    #prog_dialog = 'IrfanView 64-bit Setup'
+    return prog_dialog
+
+
+def set_button_list(button_values):
+    buttons = button_values[0][1]
+    button_list = buttons.split(",")
+    print(button_list)
+    #button_list = ['Weiter >', 'Weiter >', 'Weiter >', 'Weiter >', ['IrfanView FAQs (Häufig gestellte Fragen) Webseite anzeigen', 'IrfanView starten', 'Fertig stellen']]
+    return button_list
+
+
+def run_prog_irfan(app, button_list, prog_dialog):
+    irfan_button_custom = button_list[-1].split(".")
+    button_list.remove(button_list[-1])
+    button_list.append(irfan_button_custom)
+    for i in button_list:
+        if i == "Weiter >":
+            app.Dialog["Weiter >"].click()
+            print(app.Dialog.print_control_identifiers())
+            app.Dialog.wait('ready', timeout=20)
+        if type(i) is list:
+            app.Dialog["IrfanView FAQs (Häufig gestellte Fragen) Webseite anzeigen"].click()
+            app.Dialog["IrfanView starten"].click()
+            app.Dialog["Fertig stellen"].click()
+
+
+def main():
+    path_values, name_values, button_values = get_db_values()
+    print(get_db_values())
+    path = set_path(path_values)
+    prog_dialog = set_prog_dialog(name_values)
+    button_list = set_button_list(button_values)
+    app = Application(backend="uia").start(path)
+    if prog_dialog == "IrfanView Setup":
+        run_prog_irfan(app, button_list, prog_dialog)
+    #print(app.windows())
+
+
+if __name__ == '__main__':
+    main()
+""" Diese Funktion soll ein Benutzen von Adminrechten erzwingen, allerdings ist eine manuelle Bestätigung nötig, außerdem startet das Setup dann nicht korrekt    
+    if is_admin():
+        main()
+    else:
+        # Re-run the program with admin rights
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1) """
