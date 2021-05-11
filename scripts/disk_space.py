170b9d5a6d4bd0142736bd4dacf045214b0bbae6
GeAd <gead23@gmx.de>
2021-05-11 12:54:45 +0000

    Disk Space Prüfung

---
+++ scripts/disk_space.py
@@ -0,0 +1,14 @@
+import psutil;
+import sys;
+
+def disk_space_free(pfad):
+    try:
+        diskspace = psutil.disk_usage(pfad)[3];
+        diskspace = 100.0 - diskspace;
+    except:
+        diskspace = "None";
+    print(str(diskspace)+"% freier Speicherplatz.");
+    return diskspace;
+
+disk_space_free(sys.argv[1]);
+#disk_space_free("D:\\"); #Debug
